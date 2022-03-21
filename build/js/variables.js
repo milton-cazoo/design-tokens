771980251c0296d37f75af9b9b68be97ffad1b7e
milton-cazoo <milton-cazoo@users.noreply.github.com>
2022-03-21 11:28:00 +0000

    Update Tokens

---
+++ build/js/variables.js
@@ -0,0 +1,20 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 21 Mar 2022 11:27:59 GMT
+ */
+
+export const AliasesPrimaryMain = "#f4970a";
+export const AliasesPrimaryLight = "#ffce85";
+export const AliasesPrimaryDark = "#c78523";
+export const AliasesSecondaryLight = "#a4d1ea";
+export const AliasesSecondaryMain = "#2d91c9";
+export const AliasesSecondaryDark = "#1a5475";
+export const ScalesOrange100 = "#ffce85";
+export const ScalesOrange500 = "#f4970a";
+export const ScalesOrange900 = "#c78523";
+export const ScalesBlue100 = "#a4d1ea";
+export const ScalesBlue500 = "#2d91c9";
+export const ScalesBlue900 = "#1a5475";
+export const ScalesPink100 = "#e0a3ea";
+export const ScalesPink500 = "#b32dc9";
+export const ScalesPink900 = "#681a75";
