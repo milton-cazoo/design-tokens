7ff52ea311218f30007713ac8d2206736ed05ef1
milton-cazoo <milton-cazoo@users.noreply.github.com>
2022-03-21 15:23:18 +0000

    Update Tokens

---
+++ js/aliases.js
@@ -0,0 +1,11 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 21 Mar 2022 15:23:17 GMT
+ */
+
+export const primaryMain = "#f4970a";
+export const primaryLight = "#ffce85";
+export const primaryDark = "#c78523";
+export const secondaryLight = "#a4d1ea";
+export const secondaryMain = "#2d91c9";
+export const secondaryDark = "#1a5475";
---
+++ js/scales.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 21 Mar 2022 15:23:17 GMT
+ */
+
+export const orange100 = "#ffce85";
+export const orange500 = "#f4970a";
+export const orange900 = "#c78523";
+export const blue100 = "#a4d1ea";
+export const blue500 = "#2d91c9";
+export const blue900 = "#1a5475";
+export const pink100 = "#e0a3ea";
+export const pink500 = "#b32dc9";
+export const pink900 = "#681a75";
