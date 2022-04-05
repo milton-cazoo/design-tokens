e603b05453b6703d1a9e40061bc12a5f7311e330
Milton Boos Junior <89074499+milton-cazoo@users.noreply.github.com>
2022-04-05 14:17:05 +0100

    fix: release config to update package version

--- packages/design-tokens/release.config.js
+++ packages/design-tokens/release.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   branches: ["main"],
   tagFormat: "${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    "@semantic-release/github",
+  ],
 };
